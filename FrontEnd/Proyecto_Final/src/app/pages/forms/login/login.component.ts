d12bda542aa6928c7b7433a81438b5e557fb4cf2
Máximo Galindo <galindomaximo0@gmail.com>
2023-08-30 10:13:54 -0300

    subo login funcional

--- FrontEnd/Proyecto_Final/src/app/pages/forms/login/login.component.ts
+++ FrontEnd/Proyecto_Final/src/app/pages/forms/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  
+  loginForm = this.formBuilder.group({
+    email:['', [Validators.required , Validators.email] ],
+    password: ['',Validators.required]
+  });
+  
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder, private authService: AuthService) { }
 
   ngOnInit() {
   }
 
+  login(){
+    console.log(this.loginForm.value)
+    this.authService.login(this.loginForm.value).subscribe({
+      next: (data) => {console.log(data);},
+      error: (error) => {console.log(error);},
+      complete:()=> {}
+    })
+  }
+
+  get email () { return this.loginForm.controls.email}
+  get password () { return this.loginForm.controls.password}
 }
---
+++ FrontEnd/Proyecto_Final/src/app/services/auth.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  url: string = "https://reqres.in/api/login";
+  constructor(private http: HttpClient) { }
+
+  login(loginRequest:any):Observable<any>
+  {
+    return this.http.post(this.url,loginRequest)
+  }
+}
