a0d4412d61bfa04c94a141b9e69fca9c532d1caf
Meritano Ignacio <137829803+Meritano-Ignacio@users.noreply.github.com>
2023-08-31 19:53:10 -0300

    Navbar con los routerlinks

--- FrontEnd/Proyecto_Final/src/app/app-routing.module.ts
+++ FrontEnd/Proyecto_Final/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/forms/login/login.component';
 import { PortafolioComponent } from './pages/portafolio/portafolio.component';
+import { LandingComponent } from './pages/landing/landing.component';
 
 const routes: Routes = [
   {path: 'login' , component: LoginComponent},
-  {path: 'portafolio', component:PortafolioComponent}
+  {path: 'portafolio', component:PortafolioComponent},
+  {path: 'home', component: LandingComponent}
 ];
 
 @NgModule({
