0c7c78bdd4849a6321e07ae69bc40c89edb52ac7
Diaz Ignacio <ignacio_dior@hotmail.com>
2023-09-03 16:38:43 -0300

    Instalación de bootstrap
    En este commit solo se definió la dependencia con bootstrap, instalado en node_modules

--- FrontEnd/Proyecto_Final/src/app/app-routing.module.ts
+++ FrontEnd/Proyecto_Final/src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/forms/login/login.component';
 import { PortafolioComponent } from './pages/portafolio/portafolio.component';
 import { LandingComponent } from './pages/landing/landing.component';
+import { ComprarAccionesComponent } from './pages/comprarAcciones/comprarAcciones.component';
 
 const routes: Routes = [
   {path: 'login' , component: LoginComponent},
   {path: 'portafolio', component:PortafolioComponent},
-  {path: 'home', component: LandingComponent}
+  {path: 'home', component: LandingComponent},
+  {path: 'comprar-acciones', component: ComprarAccionesComponent}
 ];
 
 @NgModule({
