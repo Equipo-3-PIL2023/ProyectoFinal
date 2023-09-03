b2a55ab3f67f910d01f1fc447c0539ea5f51b3f4
Stéfano Frighetto <frighettostefano@gmail.com>
2023-09-03 13:00:17 -0300

    Template y CSS de Landing. Algunas modificaciones en routing

--- FrontEnd/Proyecto_Final/src/app/app-routing.module.ts
+++ FrontEnd/Proyecto_Final/src/app/app-routing.module.ts
@@ -3,11 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/forms/login/login.component';
 import { PortafolioComponent } from './pages/portafolio/portafolio.component';
 import { LandingComponent } from './pages/landing/landing.component';
+import { RegistrerComponent } from './pages/forms/registrer/registrer.component';
+import { ComprarAccionesComponent } from './pages/comprarAcciones/comprarAcciones.component';
 
 const routes: Routes = [
+  {path: '', component: LandingComponent },
   {path: 'login' , component: LoginComponent},
   {path: 'portafolio', component:PortafolioComponent},
-  {path: 'home', component: LandingComponent}
+  {path: 'home', component: LandingComponent},
+  {path: 'register', component: RegistrerComponent},
+  {path: 'comprar', component: ComprarAccionesComponent}
 ];
 
 @NgModule({
