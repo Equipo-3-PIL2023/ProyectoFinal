84e66cc0a83097b06f80b3a9ec6f9bb6f6118353
Máximo Galindo <galindomaximo0@gmail.com>
2023-09-05 11:25:25 -0300

    subo avances

--- FrontEnd/Proyecto_Final/src/app/pages/portafolio/portafolio.component.ts
+++ FrontEnd/Proyecto_Final/src/app/pages/portafolio/portafolio.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Portafolio } from './clases/portafolio';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-portafolio',
@@ -7,9 +9,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PortafolioComponent implements OnInit {
 
-  constructor() { }
+  saldoCuenta:number = 0;
+  totalInvertido: number = 0;
+  portafolio: Portafolio[] = [
+    
+  ];
+  
+
+  constructor(private authService:AuthService) { }
 
   ngOnInit() {
+    this.authService.getPortafolio().subscribe({
+      next: (data) => {
+        console.log(data);       
+        const portafolioItem = data[0];
+        this.totalInvertido = portafolioItem.totalInvertido;
+        this.saldoCuenta = portafolioItem.saldoCuenta;
+        this.portafolio = portafolioItem.acciones
+        
+        this.portafolio = portafolioItem.acciones.map((item: any) => ({
+          accion: item.accion,
+          cantidad: item.cantidad,
+          valor: item.valor,
+          ganancia: item.ganancia,
+          perdida: item.perdida
+        }));
+        console.log(this.portafolio);
+      },
+      error: (error) => {console.log(error);},
+      complete:()=> {}
+    })
   }
+
 
 }
